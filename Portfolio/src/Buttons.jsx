5ebf600e05ecd660ea6cd755b587528f468b2b89
Kisahomi <magnemite72@gmail.com>
2025-02-22 23:16:48 +0800

    more updates

--- Portfolio/src/Buttons.jsx
+++ Portfolio/src/Buttons.jsx
@@ -1,16 +1,25 @@
 import './Buttons.css'
+import React, {useState} from 'react';
 
 function Buttons(){
 
-
+    const [buttonStatus, setStatus] = useState(false);
+    const toggleStatus = () =>  {
+        setStatus(!buttonStatus);
+    }
     return (
         <>
             <div className='buttondiv'>
-                <button> About me </button>
-                <button> Resume </button>
+                <button onClick={() => toggleStatus()}> About me </button>
+                <button> {buttonStatus ? "on" : "off"} </button>
                 <button> Portfolio </button>
                 <button> Contact </button>        
             </div>
+            {buttonStatus && 
+            <div className='pop-up'>
+                hello
+            </div>
+            }
         </>
     );
 
--- Portfolio/src/Introduction.jsx
+++ Portfolio/src/Introduction.jsx
@@ -5,9 +5,21 @@ function Introduction() {
   
   return(
    <>
-   <div>
-      
-   </div>
+      <div className='introdiv'>
+          <h1>
+            <span class="colorgray">&lt;</span><span class="colorblue">div</span><span class="colorgray">&gt;</span>
+            <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;<span class="colorgray">&lt;</span><span class="colorblue">h1</span><span class="colorgray">&gt;</span>
+            <span class="colorwhite">Portfolio</span>
+            <span class="colorgray">&lt;/</span><span class="colorblue">h1</span><span class="colorgray">&gt;</span>
+            <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;<span class="colorgray">&lt;</span><span class="colorblue">h2</span><span class="colorgray">&gt;</span>
+            <span class="colorwhite">Lucas Jemuel</span>
+            <span class="colorgray">&lt;/</span><span class="colorblue">h2</span><span class="colorgray">&gt;</span>
+            <br/>
+            <span class="colorgray">&lt;/</span><span class="colorblue">div</span><span class="colorgray">&gt;</span>
+          </h1>
+      </div>
 
    </>
   );
